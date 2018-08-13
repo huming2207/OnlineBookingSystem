7ed4dcd6b23cd7f3d1edc37042941821f1949d34
Jackson Ming Hu <huming2207@gmail.com>
2018-08-13 15:18:08 +1000

    Test: no MockMvc is allowed

--- src/main/test/OnlineBookingSystem/LoginControllerTest.java
+++ src/main/test/OnlineBookingSystem/LoginControllerTest.java
@@ -1,14 +1,24 @@
 package OnlineBookingSystem;
 
 import OnlineBookingSystem.Controllers.LoginController;
+import OnlineBookingSystem.ModelClasses.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+import javax.servlet.http.HttpSession;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -16,50 +26,70 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-@AutoConfigureMockMvc
 public class LoginControllerTest
 {
     @Autowired
-    private MockMvc mockMvc;
+    private LoginController loginController;
 
     @Test
-    public void performValidAdminLoginTest() throws Exception
+    public void performValidAdminLoginTest()
     {
-        mockMvc.perform(MockMvcRequestBuilders
-                .post("/")
-                .param("username", "whatever")
-                .param("password", "1234567890"))
-                .andExpect(status().is3xxRedirection());
+        ModelAndView modelAndView = loginController.login("whatever", "1234567890");
+        HttpSession httpSession = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
+                .getRequest().getSession();
+
+        // Test roles, shouldn't be null
+        OBSFascade obs = OBSModel.getModel();
+        Role role = (Role)httpSession.getAttribute("role");
+        Assertions.assertNotNull(role);
+        Assertions.assertSame(role, Role.BusinessOwner);
+
+        // Test the user, should be a valid BusinessOwner
+        User user = obs.getBusinessOwnerById((Integer)httpSession.getAttribute("id"));
+        Assertions.assertNotNull(user);
     }
 
     @Test
     public void performValidCustomerLoginTest() throws Exception
     {
-        mockMvc.perform(MockMvcRequestBuilders
-                .post("/")
-                .param("username", "customer123")
-                .param("password", "1234567890"))
-                .andExpect(status().is3xxRedirection());
+        // Do a admin login
+        ModelAndView modelAndView = loginController.login("customer123", "1234567890");
+        HttpSession httpSession = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
+                .getRequest().getSession();
+
+        // Test roles, shouldn't be null
+        OBSFascade obs = OBSModel.getModel();
+        Role role = (Role)httpSession.getAttribute("role");
+        Assertions.assertNotNull(role);
+        Assertions.assertSame(role, Role.Customer);
+
+        // Test the user, should be a valid BusinessOwner
+        User user = obs.getCustomerById((Integer)httpSession.getAttribute("id"));
+        Assertions.assertNotNull(user);
     }
 
     @Test
     public void performInvalidAdminLoginTest() throws Exception
     {
-        mockMvc.perform(MockMvcRequestBuilders
-                .post("/")
-                .param("username", "whatever")
-                .param("password", "just_a_wrong_password"))
-                .andExpect(content().string(containsString("Incorrect username or password")));
+        // Do a admin login
+        ModelAndView modelAndView = loginController.login("null-admin", "not-a-valid-password");
+        HttpSession httpSession = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
+                .getRequest().getSession();
+
+        // Test roles, shouldn't be null
+        OBSFascade obs = OBSModel.getModel();
+        Role role = (Role)httpSession.getAttribute("role");
+        Assertions.assertNull(role);
+
+        // Test the user, should be a valid BusinessOwner
+        User user = obs.getCustomerById((Integer)httpSession.getAttribute("id"));
+        Assertions.assertNotNull(user);
     }
 
     @Test
     public void performInvalidCustomerLoginTest() throws Exception
     {
-        mockMvc.perform(MockMvcRequestBuilders
-                .post("/")
-                .param("username", "customer123")
-                .param("password", "just_a_wrong_password"))
-                .andExpect(content().string(containsString("Incorrect username or password")));
+
     }
 
 }
